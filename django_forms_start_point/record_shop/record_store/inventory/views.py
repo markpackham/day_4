9da5cd57dc915436eb1ea5a84519d52d52a5a149
mark <markpackham1@gmail.com>
2020-08-06 14:27:23 +0100

    create forms

--- django_forms_start_point/record_shop/record_store/inventory/views.py
+++ django_forms_start_point/record_shop/record_store/inventory/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from inventory.models import *
+from .forms import AlbumForm
 
 def index(request):
     artists = Artist.objects.all()
     return render(request, "inventory/index.html", locals())
+def album_new(request):
+    form = AlbumForm()
+    return render(request, "inventory/album_form.html", {'form': form})
