9db0f8da863094c34506f4d0ed1d74704529c5c8
mark <markpackham1@gmail.com>
2020-08-06 14:38:15 +0100

    finish off saving form to database

--- django_forms_start_point/record_shop/record_store/inventory/views.py
+++ django_forms_start_point/record_shop/record_store/inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from inventory.models import *
 from .forms import AlbumForm
@@ -6,6 +6,14 @@ from .forms import AlbumForm
 def index(request):
     artists = Artist.objects.all()
     return render(request, "inventory/index.html", locals())
+
 def album_new(request):
-    form = AlbumForm()
-    return render(request, "inventory/album_form.html", {'form': form})
+    if request.method == "POST":
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            album = form.save(commit=False)
+            album.save()
+            return redirect('index')
+    else:
+        form = AlbumForm()
+        return render(request, "inventory/album_form.html", {'form': form})
