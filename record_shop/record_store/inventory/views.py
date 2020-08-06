17d325897c0232402e83d76cfa93af1d896baa24
mark <markpackham1@gmail.com>
2020-08-06 13:22:04 +0100

    output from template

--- record_shop/record_store/inventory/views.py
+++ record_shop/record_store/inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello world! Inventory Index!")
+    #return HttpResponse("Hello world! Inventory Index!")
+    return render(request, "inventory/index.html")
 # Create your views here.
--- record_shop/record_store/record_store/settings.py
+++ record_shop/record_store/record_store/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'inventory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'record_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
